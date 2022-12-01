d68fdf7ca8f7eeb279b8b34b9b1d732453fae347
Yousset Chaco <davidbelandria126@gmail.com>
2022-12-01 15:59:10 -0500

    Adding Security

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require('firebase-functions');
+const firebaseAdmin = require('firebase-admin');
 const express = require('express');
+
 const app = express();
+const firebaseApp = firebaseAdmin.initializeApp();
 
 const people = [
     {
@@ -20,7 +23,37 @@ const people = [
     }
 ];
 
+const appCheckVerification = async (req, res, next) => {
+    if (next.app == undefined) {
+        res.status(412);
+        return next('The function must be called from an App Check verified app.');
+    }
+
+    const appCheckToken = req.header('X-Firebase-AppCheck');
+
+    if (!appCheckToken) {
+        res.status(401);
+        return next('Unauthorized');
+    }
+
+    try {
+        const appCheckClaims = await firebaseAdmin.appCheck().verifyToken(appCheckToken);
+        // If verifyToken() succeeds, continue with the next middleware
+        // function in the stack.
+        return next();
+    } catch (err) {
+        res.status(401);
+        return next('Unauthorized');
+    }
+}
+
 app.get('/api', (req, res) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.json({bongs: 'BONG '.repeat(hours)});
+});
+
+app.get('/api/checkAuth', [appCheckVerification], (req, res) => {
     const date = new Date();
     const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
     res.json({bongs: 'BONG '.repeat(hours)});
